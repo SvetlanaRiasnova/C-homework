a10ef5c815e7b911d8643aefa73565ecea9ef322
svetlana <R_S_U@outlook.com>
2023-01-23 23:06:39 +1000

    добавлено решение задачи 1 второго домашнего задания

---
+++ home2001/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трехзначное число");
+string num = Console.ReadLine();
+int number = Convert.ToInt32(num);
+
+//метод нахождения второй цифры трехзначного числа
+int FindSecondNumber(int number) {
+    int firstNumDel = number % 100;
+    int secondNum = firstNumDel / 10;
+
+ return secondNum;
+}
+
+int findNum = FindSecondNumber(number);
+Console.WriteLine($"второе число равно {findNum}");
