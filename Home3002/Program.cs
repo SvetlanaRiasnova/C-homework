56d2742d8aa93b0cc5f2f6c11a684e1bccb2b453
svetlana <R_S_U@outlook.com>
2023-01-26 22:45:25 +1000

    добавлено решение задачи 2 домашнего задания 3

---
+++ Home3002/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача 21 (branch task_2)
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+int getCoordFromUser(string message){
+    int result = 0;
+    while (true){
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result)){
+                break;
+        }        
+        else{
+            Console.WriteLine("You've entered incorrect number");
+        }
+    }
+    return result;
+}
+
+double getDistance(int ax, int ay, int bx, int by, int az, int bz){
+    double result = Math.Sqrt(Math.Pow(ax-bx,2) + Math.Pow(ay-by, 2) + Math.Pow(az-bz, 2));
+    return result;
+}
+
+int ax = getCoordFromUser("Enter x coordinate of the first point");
+int ay = getCoordFromUser("Enter y coordinate of the first point");
+int az = getCoordFromUser("Enter x coordinate of the second point");
+
+
+int bx = getCoordFromUser("Enter x coordinate of the second point");
+int by = getCoordFromUser("Enter y coordinate of the second point");
+int bz = getCoordFromUser("Enter y coordinate of the second point");
+
+double distance = getDistance(ax, ay, bx, by, az, bz);
+
+Console.WriteLine($"Distance between tow points is {distance:f2}");
