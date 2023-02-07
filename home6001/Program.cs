c293cf27159da8c0e3ec6e247a69b7a0c9b8b567
svetlana <R_S_U@outlook.com>
2023-02-07 18:51:55 +1000

    Добавлено решение задачи 1 домашнего заданя 6

---
+++ home6001/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 
+0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+вводим через один Enter (разбиваем через Split())
+*/
+
+Console.WriteLine(" Введите числа через запятую");
+
+string nambers = Console.ReadLine();
+int[] arr = Array.ConvertAll(nambers.Split(','), int.Parse);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0) {
+        count+=1;
+    }
+}
+
+Console.WriteLine($"пользователь ввел {count} чисел больше 0");
